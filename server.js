87e5c06ff5c0f2c24a58d801522cf7519baa1b09
shovan777 <raj.shrestha778@gmail.com>
2018-09-14 15:21:05 +0545

    Listen to chatme on server

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-
-
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise
 
 const bodyParser = require('body-parser');
-
 
 var app = express();
 var server = http.createServer(app);
@@ -18,21 +15,22 @@ const port = process.env.PORT || 3020;
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 
 var username = 'raj778';
 // var encoded_user = encodeURIComponent(username);
 var password = 'r@j777';
-var connectPath, options;
+var connectPath,
+  options;
 
 connectPath = "mongodb://ds117719.mlab.com:17719/todoapp";
-  options = {
-    useNewUrlParser: true,
-    auth: {
-      user: username,
-      password: password
-    }
+options = {
+  useNewUrlParser: true,
+  auth: {
+    user: username,
+    password: password
   }
+}
 // var conString = "mongodb://raj778:r@j777@ds117719.mlab.com:17719/todoapp";
 
 app.use(express.static(__dirname))
@@ -44,95 +42,82 @@ app.use(express.static(__dirname))
 // var Chats = mongoose.model("Chats", ChatSchema)
 //
 
-
-
 mongoose.connect(connectPath, options).then(() => {
   console.log('mongodb connected');
-},
-(e) => {
+}, (e) => {
   console.log(e);
 });
 
-var schema = new mongoose.Schema({ name: 'string', chat: 'string' });
+var schema = new mongoose.Schema({name: 'string', color: 'string', messsage: 'string'});
 var Chats = mongoose.model('Chats', schema);
 
 io.on('connection', (socket) => {
   console.log('New user connected');
 
   socket.on("chatme", (message) => {
-      console.log("inside chatme");
-      var chatMessage = {
-        name: 'swaingade',
-        chat: message
-      };
-      
-      try {
-	console.log(chatMessage);
-        var chat = new Chats(chatMessage);
-        chat.save();
-        io.emit('chat', chatMessage);
-        
+    console.log("inside chatme");
+    // var chatMessage = {
+    //   name: 'swaingade',
+    //   message: message
+    // };
+
+    try {
+      console.log(chatMessage);
+      var chat = new Chats(chatMessage);
+      chat.save();
+      io.emit('chat', chatMessage);
+
     } catch (error) {
 
-        console.error(error);
+      console.error(error);
 
     }
-      
 
-    });
+  });
 
   socket.on('disconnect', () => {
     console.log('User was disconnected');
   });
 });
 
-
 app.post("/chats", async (req, res) => {
 
-    try {
-        console.log(req.body);
-        var chat = new Chats(req.body)
+  try {
+    console.log(req.body);
+    var chat = new Chats(req.body)
 
-        await chat.save();
-        // console.log('inside post');
-        // console.log(`posting chat: ${chat}`);
+    await chat.save();
+    // console.log('inside post');
+    // console.log(`posting chat: ${chat}`);
 
+    res.sendStatus(200)
 
-        res.sendStatus(200)
+    // Emit chat event
+    io.emit('chat', req.body);
+    // io.emit('chatme', hello);
 
-        // Emit chat event
-        io.emit('chat', req.body);
-        // io.emit('chatme', hello);
+  } catch (error) {
 
-    } catch (error) {
+    res.sendStatus(500);
 
-        res.sendStatus(500);
+    console.error(error);
 
-        console.error(error);
-
-    }
+  }
 
 });
 
 app.get("/chats", (req, res) => {
 
-    Chats.find({}, (error, chats) => {
+  Chats.find({}, (error, chats) => {
 
-        res.send(chats)
+    res.send(chats)
 
-    });
+  });
 
 });
 
-
-
-
-
-
-
-
 server.listen(port, () => {
 
-    console.log("Well done, now I am listening...")
+  console.log("Well done, now I am listening...")
 
 })
